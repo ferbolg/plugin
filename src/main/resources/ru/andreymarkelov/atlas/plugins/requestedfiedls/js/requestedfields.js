28797680ebe73aabfe69d0af282b3ac19d523c9f
Serhii Zinchuk <ferbolg@gmail.com>
2017-03-02 17:41:34 +0200

    Working JS fix for hard-coded id

---
+++ src/main/resources/ru/andreymarkelov/atlas/plugins/requestedfiedls/js/requestedfields.js
@@ -0,0 +1,29 @@
+
+    (function($) {
+        $(document).ready(function() {
+            AJS.$("#customfield_10000 option[value='-1']").remove(); //Removes the default value "None"
+            function convertMulti(id) {
+                var myVar = 234;
+                console.log(myVar);
+                if (AJS.$('#' + id + "-textarea").length == 0) {
+                    new AJS.MultiSelect({
+                        element: $("#" + id),
+                        itemAttrDisplayed: "label",
+                        errorMessage: AJS.params.multiselectComponentsError
+                    });
+                }
+            }
+
+            AJS.toInit(function () {
+                convertMulti("customfield_10000");
+            })
+
+            JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context) {
+                AJS.$("#customfield_<10000> option[value='-1']").remove();
+                convertMulti("customfield_<10000>");
+            });
+        });
+    })(AJS.$ || jQuery);
+
+    // var myVar = 234;
+    // console.log(myVar);
